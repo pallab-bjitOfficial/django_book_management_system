8ba2dcce8ae15059efdd1d7a85d881aa7f77bd3a
Pallab Majumdar <155512458+pallab-bjitOfficial@users.noreply.github.com>
2024-09-09 16:49:29 +0600

    added a base model

--- author/models.py
+++ author/models.py
@@ -1,13 +1,10 @@
 from django.db import models
+from book_management_system.model import BaseModel
 
-# Create your models here.
 
-
-class Author(models.Model):
+class Author(BaseModel):
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- book/models.py
+++ book/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from author.models import Author
-# Create your models here.
+from book_management_system.model import BaseModel
 
 
-class Book(models.Model):
+class Book(BaseModel):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     author = models.ForeignKey(
         Author, on_delete=models.CASCADE, related_name='books')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
