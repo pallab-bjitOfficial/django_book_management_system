eddf776ae3f167ff319892cc1ed0b8295df5d614
Pallab Majumdar <155512458+pallab-bjitOfficial@users.noreply.github.com>
2024-09-09 14:55:42 +0600

    author update and delete

--- author/views.py
+++ author/views.py
@@ -27,3 +27,17 @@ class AuthorViewSet(viewsets.ModelViewSet):
         author = Author.objects.get(pk=pk)
         serializer = AuthorSerializer(author)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(
+            author, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, _, pk=None):
+        author = Author.objects.get(pk=pk)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
